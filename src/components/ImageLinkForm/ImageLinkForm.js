3f74285beeb01996b6a6bbc66a7d6c7a086dcb96
Daniel Scalettar <scalettar+github@gmail.com>
2019-08-04 00:56:10 -0700

    Change some text

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,7 +5,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
     return (
         <div>
             <p className='f3'>
-                {'Hello. I am SmartBrain. Upload a picture and I will attempt to recognize faces.'}
+                {'Hello. I am Facial Recognition Brain. Upload a picture and I will attempt to recognize faces.'}
             </p>
             <div className='center'>
                 <div className='form center pa4 br3 shadow-5'>
